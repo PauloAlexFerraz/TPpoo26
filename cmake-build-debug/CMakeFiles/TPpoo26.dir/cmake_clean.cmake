2ce43bfb0ddfa3851db2eb87fb0f5153cef3014d
LemosPT <guilhermelemo@gmail.com>
2025-10-13 17:50:28 +0100

    3

---
+++ cmake-build-debug/CMakeFiles/TPpoo26.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TPpoo26.dir/Jardim.cpp.obj"
+  "CMakeFiles/TPpoo26.dir/Jardim.cpp.obj.d"
+  "CMakeFiles/TPpoo26.dir/main.cpp.obj"
+  "CMakeFiles/TPpoo26.dir/main.cpp.obj.d"
+  "TPpoo26.exe"
+  "TPpoo26.exe.manifest"
+  "TPpoo26.pdb"
+  "libTPpoo26.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TPpoo26.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TPpoo26.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TPpoo26.
+# This may be replaced when dependencies are built.
