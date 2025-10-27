f389c40632f8cf19ae02edc8cab625c55cf95305
LemosPT <guilhermelemo@gmail.com>
2025-10-27 19:35:22 +0000

    c

---
+++ cmake-build-debug/CMakeFiles/TPpoo26.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/guilh/CLionProjects/TPpoo27/Cacto.cpp" "CMakeFiles/TPpoo26.dir/Cacto.cpp.obj" "gcc" "CMakeFiles/TPpoo26.dir/Cacto.cpp.obj.d"
+  "C:/Users/guilh/CLionProjects/TPpoo27/Comandos.cpp" "CMakeFiles/TPpoo26.dir/Comandos.cpp.obj" "gcc" "CMakeFiles/TPpoo26.dir/Comandos.cpp.obj.d"
+  "C:/Users/guilh/CLionProjects/TPpoo27/ErvaDaninha.cpp" "CMakeFiles/TPpoo26.dir/ErvaDaninha.cpp.obj" "gcc" "CMakeFiles/TPpoo26.dir/ErvaDaninha.cpp.obj.d"
+  "C:/Users/guilh/CLionProjects/TPpoo27/Jardim.cpp" "CMakeFiles/TPpoo26.dir/Jardim.cpp.obj" "gcc" "CMakeFiles/TPpoo26.dir/Jardim.cpp.obj.d"
+  "C:/Users/guilh/CLionProjects/TPpoo27/Orquidia.cpp" "CMakeFiles/TPpoo26.dir/Orquidia.cpp.obj" "gcc" "CMakeFiles/TPpoo26.dir/Orquidia.cpp.obj.d"
+  "C:/Users/guilh/CLionProjects/TPpoo27/Planta.cpp" "CMakeFiles/TPpoo26.dir/Planta.cpp.obj" "gcc" "CMakeFiles/TPpoo26.dir/Planta.cpp.obj.d"
+  "C:/Users/guilh/CLionProjects/TPpoo27/Roseira.cpp" "CMakeFiles/TPpoo26.dir/Roseira.cpp.obj" "gcc" "CMakeFiles/TPpoo26.dir/Roseira.cpp.obj.d"
+  "C:/Users/guilh/CLionProjects/TPpoo27/Solo.cpp" "CMakeFiles/TPpoo26.dir/Solo.cpp.obj" "gcc" "CMakeFiles/TPpoo26.dir/Solo.cpp.obj.d"
+  "C:/Users/guilh/CLionProjects/TPpoo27/adubo.cpp" "CMakeFiles/TPpoo26.dir/adubo.cpp.obj" "gcc" "CMakeFiles/TPpoo26.dir/adubo.cpp.obj.d"
+  "C:/Users/guilh/CLionProjects/TPpoo27/main.cpp" "CMakeFiles/TPpoo26.dir/main.cpp.obj" "gcc" "CMakeFiles/TPpoo26.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TPpoo26.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TPpoo26.dir/Cacto.cpp.obj"
+  "CMakeFiles/TPpoo26.dir/Cacto.cpp.obj.d"
+  "CMakeFiles/TPpoo26.dir/Comandos.cpp.obj"
+  "CMakeFiles/TPpoo26.dir/Comandos.cpp.obj.d"
+  "CMakeFiles/TPpoo26.dir/ErvaDaninha.cpp.obj"
+  "CMakeFiles/TPpoo26.dir/ErvaDaninha.cpp.obj.d"
+  "CMakeFiles/TPpoo26.dir/Jardim.cpp.obj"
+  "CMakeFiles/TPpoo26.dir/Jardim.cpp.obj.d"
+  "CMakeFiles/TPpoo26.dir/Orquidia.cpp.obj"
+  "CMakeFiles/TPpoo26.dir/Orquidia.cpp.obj.d"
+  "CMakeFiles/TPpoo26.dir/Planta.cpp.obj"
+  "CMakeFiles/TPpoo26.dir/Planta.cpp.obj.d"
+  "CMakeFiles/TPpoo26.dir/Roseira.cpp.obj"
+  "CMakeFiles/TPpoo26.dir/Roseira.cpp.obj.d"
+  "CMakeFiles/TPpoo26.dir/Solo.cpp.obj"
+  "CMakeFiles/TPpoo26.dir/Solo.cpp.obj.d"
+  "CMakeFiles/TPpoo26.dir/adubo.cpp.obj"
+  "CMakeFiles/TPpoo26.dir/adubo.cpp.obj.d"
+  "CMakeFiles/TPpoo26.dir/main.cpp.obj"
+  "CMakeFiles/TPpoo26.dir/main.cpp.obj.d"
+  "TPpoo26.exe"
+  "TPpoo26.exe.manifest"
+  "TPpoo26.pdb"
+  "libTPpoo26.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TPpoo26.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
